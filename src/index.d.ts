6f5824141c16227a413a57a7f9f6f7deabfa450e
orbitingflea <orbitingflea@qq.com>
2022-02-12 19:57:04 +0800

    clean up

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,25 +1,66 @@
-interface Memory {
-  creepTmpRequire?: { [name: string]: number };
-}
+export {};
 
-interface CreepMemory {
-  target?: any;  // to be deleted
-  driveInfo?: any; // debug
-}
+declare global {
+  interface Memory {
+    creepTmpRequire?: { [name: string]: number };
+  }
 
-interface Creep {
-  work(): void;
-  park(): void;
-}
+  interface CreepMemory {
+    target?: any;  // to be deleted
+    driveInfo?: any; // debug
+  }
 
-interface RoomMemory {
-  _spawnedDefender?: boolean;
-}
+  interface Creep {
+    work(): void;
+    park(): void;
+  }
 
-interface RoomObjectCache {
-  isContainerNearController?: boolean;
-}
+  interface RoomMemory {
+    _spawnedDefender?: boolean;
+  }
 
-// declare var creepManager: any;
-declare var decodeRoomPosition: (code: string) => RoomPosition;
-declare const roomDanger: (roomName: string) => DangerInfo | null;
+  interface RoomObjectCache {
+    isContainerNearController?: boolean;
+  }
+
+  namespace NodeJS {
+    interface Global {
+      tickBeginHook: (() => void)[];
+      tickEndHook: (() => void)[];
+      creepManager: CreepManager;
+      CarrierManager: any;
+
+      staticCache: {
+        data: { [key: string]: { updateTime: number, value: Object } };
+        get: (key: string, ttl?: number) => Object | null;
+        set: (key: string, value: Object) => void;
+      };
+
+      creepCache: {
+        data: { [id: string]: Object };
+        get: (id: Id<Creep>) => Object | null;
+        set: (id: Id<Creep>, value: Object) => void;
+      };
+
+      _roomCache: {
+        data: { [name: string]: RoomCache };
+        get: (name: string) => RoomCache;
+        set: (name: string, value: RoomCache) => void;
+      };
+      roomCache: (name: string) => RoomCache;
+
+      roomObjectCache: {
+        data: { [id: string]: Object };
+        get: (id: Id<RoomObject>) => Object | null;
+        set: (id: Id<RoomObject>, value: Object) => void;
+      };
+
+      roomDanger: (roomName: string) => DangerInfo | null;
+      roomLairRegions: (roomName: string) => LairRegionStatic[];
+      decodeRoomPosition: (code: string) => RoomPosition;
+    }
+  }
+
+  let decodeRoomPosition: (code: string) => RoomPosition;
+  const roomDanger: (roomName: string) => DangerInfo | null;
+}
